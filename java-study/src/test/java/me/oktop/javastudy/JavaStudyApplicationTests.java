3ebab67a5f3acf9bb3a7566a6b940eadb420b7cf
oktopman <colaws3@gmail.com>
2020-11-29 00:13:35 +0900

    initialize commit

---
+++ java-study/src/test/java/me/oktop/javastudy/JavaStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package me.oktop.javastudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
