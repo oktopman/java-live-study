f609ca69232d32cb338f18b959ba5ff739a7d7a0
oktopman <colaws3@gmail.com>
2020-12-03 00:50:25 +0900

    repository issue 댓글단사람의 이름과 참여율 구하기(중복참여자는 카운팅 하지않음)

--- java-study/src/test/java/me/oktop/javastudy/week4/dashboard/DashboardServiceTest.java
+++ java-study/src/test/java/me/oktop/javastudy/week4/dashboard/DashboardServiceTest.java
@@ -7,9 +7,8 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.io.IOException;
-import java.util.List;
-
-import static org.assertj.core.api.Assertions.assertThat;
+import java.util.HashMap;
+import java.util.Map;
 
 @SpringBootTest
 class DashboardServiceTest {
@@ -22,10 +21,30 @@ class DashboardServiceTest {
     void get_github_repo_issue() throws IOException {
         GitHub gitHub = new GitHubBuilder().withOAuthToken(token).build();
         GHRepository ghRepository = gitHub.getRepository("oktopman/java-live-study");
-        GHIssue issue = ghRepository.getIssue(2);
-        List<GHIssueComment> comments = issue.getComments();
-        assertThat(comments.get(0).getBody()).isEqualTo("댓글2입니다~");
+        Map<String, Integer> participantMap = generateParticipants(ghRepository);
+
+        for (Map.Entry<String, Integer> element : participantMap.entrySet()) {
+            double participationPercent = (element.getValue() * 100) / 18.0;
+            System.out.println(String.format("참여자 : %s 참여율 : %.2f", element.getKey(), participationPercent));
+        }
     }
 
+    private Map<String, Integer> generateParticipants(GHRepository ghRepository) throws IOException {
+        Map<String, Integer> participantMap = new HashMap<>();
+        for (int i = 1; i <= 2; i++) {
+            GHIssue issue = ghRepository.getIssue(i);
+            PagedIterable<GHIssueComment> ghIssueComments = issue.listComments();
+            String temp = "";
+            for (GHIssueComment comment : ghIssueComments) {
+                String username = comment.getUser().getLogin();
+                if (temp.equals(username))
+                    continue;
+
+                temp = username;
+                participantMap.put(username, participantMap.getOrDefault(username, 0) + 1);
+            }
+        }
+        return participantMap;
+    }
 
 }
