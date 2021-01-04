68c40a977b418a7c30d9b53080a2fd3d9cd01a80
oktopman <colaws3@gmail.com>
2021-01-05 01:01:26 +0900

    인터페이스 정의하는 방법

---
+++ java-study/src/main/java/me/oktop/javastudy/week8/Bank.java
@@ -0,0 +1,8 @@
+package me.oktop.javastudy.week8;
+
+import java.util.Map;
+
+public interface Bank {
+
+    Map<String, Object> sendData(SendDto sendDto);
+}
---
+++ java-study/src/main/java/me/oktop/javastudy/week8/SendDto.java
@@ -0,0 +1,6 @@
+package me.oktop.javastudy.week8;
+
+public class SendDto {
+    private String name;
+    private long amount;
+}
